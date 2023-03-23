787ccf2586943bbe9b024d779337966813c1ac73
Sdero05 <nikolarydoi@gmail.com>
2023-03-23 17:41:28 +0200

    homework2

--- HomeWork1/index.js
+++ HomeWork1/index.js
@@ -1,3 +1,17 @@
 
 let name = prompt("Your name?:");
 alert(`Hello, ${name}! How are you?`);
+
+
+const a = [1,2,3,4];
+console.log(a.length);
+
+const test = 'test'
+const testArr = test.split('')
+testArr[2] = testArr[2].toUpperCase()
+const finishedStr = testArr.join('')
+console.log(finishedStr)
+
+
+
+
