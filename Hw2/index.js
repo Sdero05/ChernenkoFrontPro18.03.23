3d3db0e8fb7760b3cc3f974d4c3ef845c7eb768e
Sdero05 <nikolarydoi@gmail.com>
2023-03-24 19:04:16 +0200

    homework2

---
+++ Hw2/index.js
@@ -0,0 +1,12 @@
+const name =prompt('Your name?')
+const surname =prompt('Your surname?')
+const old =prompt('How old are you?')
+const str = `I am ${name} ${surname} ${old}`
+console.log(str.toString().split('1').join('1'))
+// 1 Задание
+
+
+
+// const number = 12345
+// console.log(number.toString().split('').join(' '))
+// 2 Задание
