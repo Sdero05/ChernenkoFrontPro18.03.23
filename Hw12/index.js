d997c89020b771a752b49518ae94000845ae3934
Sdero05 <nikolarydoi@gmail.com>
2023-04-09 22:01:19 +0300

    homework12

--- Hw12/index.js
+++ Hw12/index.js
@@ -56,6 +56,8 @@
 
 
 // console.log(`Кількість непарних позитивних елементів: ${countOddPositive}`);
+
+
 // let arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 // let positiveArr = arr.filter((num) => num > 0);
 // let evenPositiveArr = positiveArr.filter((num) => num % 2 === 0);
